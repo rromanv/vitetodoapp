8301e08caed3f29c08742284582dc441bb34b00d
Rodolfo Roman <rromanv@gmail.com>
2021-11-27 00:14:55 -0600

    minor lint

--- src/composables/useTodos.js
+++ src/composables/useTodos.js
@@ -1,30 +1,30 @@
-import { ref, computed } from "vue";
+import { ref, computed } from 'vue'
 
-const todos = ref([]);
+const todos = ref([])
 
 const useTodos = () => {
   const pending = computed(() => {
-    return todos.value.filter((todo) => !todo.done);
-  });
+    return todos.value.filter(todo => !todo.done)
+  })
 
   const completed = computed(() => {
-    return todos.value.filter((todo) => todo.done);
-  });
+    return todos.value.filter(todo => todo.done)
+  })
 
-  const addTodo = (newTodo) => {
+  const addTodo = newTodo => {
     if (newTodo.trim()) {
       todos.value.push({
         id: todos.value.length,
         content: newTodo,
         done: false,
-      });
+      })
     }
-  };
+  }
 
-  const changeStatus = (id) => {
-    const todo = todos.value.find((todo) => todo.id === id);
-    todo.done = !todo.done;
-  };
+  const changeStatus = id => {
+    const todo = todos.value.find(todo => todo.id === id)
+    todo.done = !todo.done
+  }
 
   return {
     todos,
@@ -32,7 +32,7 @@ const useTodos = () => {
     completed,
     addTodo,
     changeStatus,
-  };
-};
+  }
+}
 
-export default useTodos;
+export default useTodos
